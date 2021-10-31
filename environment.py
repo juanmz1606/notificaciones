9e943aa86246a87db43b3e6d5ee43175b24817f6
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2021-10-30 20:29:32 -0500

    Creación servicio de envio de sms en phyton

--- environment.py
+++ environment.py
@@ -1,5 +1,7 @@
 import os
 
-os.environ["SENDGRID_API_KEY"]="Llave de SendGrid"
+os.environ["SENDGRID_API_KEY"]="Sendgrid API key"
 os.environ["SECURITY_HASH"]="ABC123"
 os.environ["email_from"]="juan.1701920466@ucaldas.edu.co"
+os.environ["TWILIO_ACCOUNT_SID"]="Twilio SID"
+os.environ["TWILIO_AUTH_TOKEN"]="Token Twilio"
--- servidor.py
+++ servidor.py
@@ -3,6 +3,7 @@ import os
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from flask import request
+from twilio.rest import Client
 
 app = Flask(__name__)
 
@@ -34,7 +35,34 @@ def enviarCorreo():
         print("Hash error")
         return "KO"
     
+@app.route("/SMS")
+def enviarSMS():
+    destino = request.args.get("destino")
+    mensaje = request.args.get("mensaje")
+    hashString = request.args.get("hash")
+    if  hashString == os.environ.get("SECURITY_HASH"):
+        try:
+            account_sid = os.environ['TWILIO_ACCOUNT_SID']
+            auth_token = os.environ['TWILIO_AUTH_TOKEN']
+            client = Client(account_sid, auth_token)
 
+            message = client.messages \
+                .create(
+                     body=mensaje,
+                     from_="+12058513504",
+                     to="+57"+destino
+                 )
+
+            print(message.sid)
+            print("enviado SMS")
+            return "OK"
+        except Exception as e:
+            print(e.message)
+            return "KO"
+    else:
+        print("Hash error")
+        return "KO"
+    
 
 if __name__ == "__main__":
     app.run()
