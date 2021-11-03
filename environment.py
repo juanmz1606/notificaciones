4312594facaf67bcd03ba800cbbe07876b73f963
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2021-11-02 19:21:58 -0500

    Envio de mensajes y correos actualizado

--- environment.py
+++ environment.py
@@ -1,7 +1,7 @@
 import os
 
-os.environ["SENDGRID_API_KEY"]="Sendgrid API key"
+os.environ["SENDGRID_API_KEY"]=""
 os.environ["SECURITY_HASH"]="ABC123"
 os.environ["email_from"]="juan.1701920466@ucaldas.edu.co"
-os.environ["TWILIO_ACCOUNT_SID"]="Twilio SID"
-os.environ["TWILIO_AUTH_TOKEN"]="Token Twilio"
+os.environ["TWILIO_ACCOUNT_SID"]=""
+os.environ["TWILIO_AUTH_TOKEN"]=""
